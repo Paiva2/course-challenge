b62d820fe08522bb20abbef5bfbc798cac994a34
paiva2 <joaovitor.paiva145@hotmail.com>
2023-12-10 02:25:03 -0300

    feat/deploy fixes

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "http://54.221.72.249:5000"
+    : "https://course-back.onrender.com"
 
 const api = axios.create({
   baseURL: baseUrl,
